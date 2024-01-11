abd1f64d81a1f1634e550da0edaa562969e8b439
봉수 김 <rlaqhdtn22@gmail.com>
2024-01-11 16:46:25 +0900

    docs : 과제 1~5 완료

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,10 +1,142 @@
-﻿namespace Homework
+﻿// 1. 사용자에세 숫자를 입력 받아서
+// 2. 0부터 ~숫자까지 가지는 리스트 만들기
+// 3. 0요소를 제거
+// 4. 리스트가 가지는 모든 요소들을 출력해주는 반복문 작성
+namespace Homework
 {
-    internal class Program
+    public class MyList<T>
     {
-        static void Main(string[] args)
+        private const int DefaultCapacity = 4;
+
+        private T[] items;
+        private int count;
+
+        public MyList()
         {
-            Console.WriteLine("Hello, World!");
+            items = new T[DefaultCapacity];
+            count = 0;
+        }
+        public MyList(int capacity)
+        {
+            items = new T[capacity];
+            count = 0;
+        }
+
+        public int Capacity
+        {
+            get { return items.Length; }
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public T this[int index]
+        {
+            get 
+            {
+                if (index >= count)
+                    throw new IndexOutOfRangeException();
+                return items[index];
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (count == items.Length)
+                Grow();
+
+            items[count++] = item;
+        }
+
+        private void Grow()
+        {
+            T[] newItems = new T[items.Length * 2];
+            Array.Copy(items, newItems, count);
+            items = newItems;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index > count)
+                throw new IndexOutOfRangeException();
+            if (count == items.Length)
+                Grow();
+
+            Array.Copy(items, index, items, index + 1, count - index);
+            items[index] = item;
+            ++count;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new IndexOutOfRangeException();
+
+            --count;
+            Array.Copy(items, index + 1, items, index, count - index);
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < count; ++i)
+            {
+                if (item.Equals(items[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            MyList<string> strList = new MyList<string>();
+
+            strList.Add("0번째 추가");
+            strList.Add("1번째 추가");
+            strList.Add("2번째 추가");
+            strList.Add("3번째 추가");
+            strList.Add("4번째 추가");
+            strList.Add("5번째 추가");
+            strList.Add("6번째 추가");
+            strList.Add("7번째 추가");
+
+            bool fail = strList.Remove("8번째 추가"); // false
+            bool comp = strList.Remove("4번째 추가"); // true
+
+            strList.Insert(4, "4번째로 인서트");
+            strList.Insert(6, "6번째로 인서트");
+
+            int index1 = strList.IndexOf("6번째 추가");
+            int index2 = strList.IndexOf("7번째 추가");
+
+            strList.RemoveAt(7);
+
+            strList.Add("끝에 추가");
+            strList.RemoveAt(0);
+            
+            for (int i = 0; i < strList.Count; ++i)
+            {
+                Console.WriteLine(strList[i]);
+            }
+        }
+    }
+
 }
---
+++ Homework/Starcraft.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 사용자의 입력을 받아서 없는 데이터면 추가, 있던 데이터면 삭제하는 리스트
+namespace Homework
+{
+    public class StarList<T>
+    {
+        private const int DefaultCapacity = 12;
+
+        private T[] items;
+        private int count;
+
+        public StarList()
+        {
+            items = new T[DefaultCapacity];
+            count = 0;
+        }
+
+        public int Capacity
+        {
+            get { return items.Length; }
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public T this[int index]
+        {
+            get 
+            {
+                if (index >= count)
+                    throw new IndexOutOfRangeException();
+                return items[index];
+            }
+        }
+        public void ControlClick(T item)
+        {
+            if (Remove(item))
+            {
+                Remove(item);
+            }
+            else
+            {
+                Add(item);
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (count == items.Length)
+            {
+                Console.WriteLine("더 이상 선택할 수 없습니다");
+                return;
+            }
+            items[count++] = item;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new IndexOutOfRangeException();
+
+            --count;
+            Array.Copy(items, index + 1, items, index, count - index);
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < count; ++i)
+            {
+                if (item.Equals(items[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+    internal class Starcraft
+    {
+        static void Main()
+        {
+            StarList<int> squad = new StarList<int>();
+            squad.ControlClick(1);
+            squad.ControlClick(6);
+            squad.ControlClick(7);
+            squad.ControlClick(8);
+            squad.ControlClick(3);
+            squad.ControlClick(2);
+            squad.ControlClick(7);
+            squad.ControlClick(1);
+            squad.ControlClick(6);
+            squad.ControlClick(8);
+        }
+    }
+}
