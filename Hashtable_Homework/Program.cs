d3a7303a6e369281789f3d63498ffb3ec219cc99
봉수 김 <rlaqhdtn22@gmail.com>
2024-01-18 16:19:45 +0900

    docs : 스타크래프트 치트키

---
+++ Hashtable_Homework/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Hashtable_Homework
+{
+    // 스타크래프트 치트키
+    public class CheatKey
+    {
+        private Dictionary<string, Action> cheatDic;
+
+        public void Run(string cheatKey)
+        {
+            if (cheatDic.ContainsKey(cheatKey))
+            {
+                cheatDic[cheatKey].Invoke();
+            }
+        }
+
+        private void ShowMeTheMoney()
+        {
+            Console.WriteLine("자원을 늘려주는 치트키 발동!");
+        }
+
+        private void TrereIsNoCowLevel()
+        {
+            Console.WriteLine("바로 승리합니다 치트키 발동!");
+        }
+
+        public CheatKey()
+        {
+            cheatDic = new Dictionary<string, Action>();
+            cheatDic.Add("ShowMeTheMoney", ShowMeTheMoney);
+            cheatDic.Add("ThereIsNoCowLevel", TrereIsNoCowLevel);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("치트를 입력해주세요");
+            string input = Console.ReadLine();
+            CheatKey cheatKey = new CheatKey();
+            cheatKey.Run(input);
+            Console.ReadKey();
+        }
+    }
+}
