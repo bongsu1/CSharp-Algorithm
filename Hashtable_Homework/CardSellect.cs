c4eabd314839718efce1e0a1ca4ab1f432b9e337
봉수 김 <rlaqhdtn22@gmail.com>
2024-01-19 09:32:30 +0900

    docs : 카드뽑기 수정

--- Hashtable_Homework/CardSellect.cs
+++ Hashtable_Homework/CardSellect.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 using System.Text;
 
 
@@ -7,24 +8,64 @@ namespace Hashtable_Homework
 {
     class CardSellect
     {
-        Dictionary<string, string> cards;
-
-        public int TakeCard(int k)
+        Dictionary<int, string> cards;
+        SortedSet<string> combination = new SortedSet<string>();
+        public int TakeCard(int take)
         {
-            int count = 0;
-
-            // 몇가지 조합
-            return count;
+            switch (take)
+            {
+                case 2:
+                    for (int i = 0; i < cards.Count; i++)
+                    {
+                        for (int j = 0; j < cards.Count; j++)
+                        {
+                            if (i == j) continue;
+                            combination.Add($"{cards[i]}{cards[j]}");
+                        }
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i < cards.Count; i++)
+                    {
+                        for (int j = 0; j < cards.Count; j++)
+                        {
+                            for (int k = 0; k < cards.Count; k++)
+                            {
+                                if (i == j || j == k || i == k) continue;
+                                combination.Add($"{cards[i]}{cards[j]}{cards[k]}");
+                            }
+                        }
+                    }
+                    break;
+                case 4:
+                    for (int i = 0; i < cards.Count; i++)
+                    {
+                        for (int j = 0; j < cards.Count; j++)
+                        {
+                            for (int k = 0; k < cards.Count; k++)
+                            {
+                                for (int r = 0; r < cards.Count; r++)
+                                {
+                                    {
+                                        if (i == j || j == k || i == k || i == r || j == r || k == r) continue;
+                                        combination.Add($"{cards[i]}{cards[j]}{cards[k]}{cards[r]}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
+            }
+            return combination.Count;
         }
-
-        public void DropCard(string _value)
+        public void DropCard(string _value, int count)
         {
-            cards.Add(_value, _value);
+            cards.Add(count, _value);
         }
 
         public CardSellect()
         {
-            cards = new Dictionary<string, string>();
+            cards = new Dictionary<int, string>();
         }
     }
 
@@ -35,9 +76,11 @@ namespace Hashtable_Homework
             CardSellect cardSellect = new CardSellect();
             int n = int.Parse(Console.ReadLine());
             int k = int.Parse(Console.ReadLine());
+            int count = 0;
             for (int i = 0; i < n; i++)
             {
-                cardSellect.DropCard(Console.ReadLine());
+                cardSellect.DropCard(Console.ReadLine(), count);
+                count++;
             }
             Console.WriteLine(cardSellect.TakeCard(k));
         }
